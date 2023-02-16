19523a7937c741c3beb4642174adca04397a50e3
Ahmed Olalekan <olalekanahmed6@gmail.com>
2023-02-16 20:11:44 +0100

    Task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,7 +2,10 @@
 
 /**
 * main - prints "\"programiming is like building  a multilingual puzzle"
+* Return: 0
 */
-void main(void)
-
-{ puts("\"Programming is like building a multilingual puzzle"); }
+int  main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
