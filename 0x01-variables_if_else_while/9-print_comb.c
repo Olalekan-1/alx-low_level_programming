c32c3582c2cf57affd2344b40be4d99568f62fee
Ahmed Olalekan <olalekanahmed6@gmail.com>
2023-02-18 00:37:26 +0100

    Task 9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+* main - Entry point
+* Return: Always 0 (sucess)
+*/
+
+int main(void)
+{	int x;
+	for (x = 48; x <= 57; x++)
+	{	putchar(x);
+	putchar(44);
+	putchar(32);
+	}
+	putchar(10);
+	return (0);
+}
