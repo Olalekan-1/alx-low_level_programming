c521bd22c81697f1f954fb4e811822c9805fbac9
Ahmed Olalekan <olalekanahmed6@gmail.com>
2023-02-17 22:12:42 +0100

    Task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+* main - Entry point
+* Return: always 0 (sucess)
+*/
+int main(void)
+{	int x = 0;
+
+	while (x < 10)
+	{	printf("%i", x);
+		x++;
+	}
+	putchar(10);
+	return (10);
+}
