9c5a56b1324c1634839f36793ba1f1b650dda65e
Ahmed Olalekan <olalekanahmed6@gmail.com>
2023-02-18 00:01:11 +0100

    Task 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+* main - Entry point
+* Return: Always 0 (sucess)
+*/
+
+int main(void)
+{	int x = 48;
+	while (x <= 57)
+	{	putchar(x);
+		x++;
+	}
+	putchar(10);
+	return	(0);
+}
