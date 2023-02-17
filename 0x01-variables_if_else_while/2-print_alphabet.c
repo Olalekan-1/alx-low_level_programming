d918040dc6a719dce850760d20e06791a9960af3
Ahmed Olalekan <olalekanahmed6@gmail.com>
2023-02-17 17:01:49 +0100

    Task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+* main - Entry point
+* Return: Always 0 (sucess)
+*/
+
+int main(void)
+{	int x;
+	for (x = 'a'; x <= 'z'; x++)
+	{	putchar(x);
+	putchar(10);
+	}
+	return (0);
+}
