a6aab4f139b44fefdc801039bddf876cbf568848
Ahmed Olalekan <olalekanahmed6@gmail.com>
2023-02-18 01:09:19 +0100

    Task 9

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -7,10 +7,12 @@
 
 int main(void)
 {	int x;
-	for (x = 48; x <= 57; x++)
+	for (x = 48; x < 58; x++)
 	{	putchar(x);
-	putchar(44);
+	if (x != 57)
+	{	putchar(44);
 	putchar(32);
+	}
 	}
 	putchar(10);
 	return (0);
