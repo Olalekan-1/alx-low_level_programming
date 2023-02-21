a3e529d2a3ef3250b6f6da3fd89f2d811885da15
Ahmed Olalekan <olalekanahmed6@gmail.com>
2023-02-21 18:56:17 +0100

    Task 4

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,17 @@
+#include"main.h"
+
+/**
+*_isalpha - check if charter is letter
+*@c: the character to be checked
+*Return: 1 if c is a letter, otherwise return 0
+*/
+int _isalpha(int c)
+{
+
+if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+{	return (0);
+}
+else
+{	return (1);
+}
+}
