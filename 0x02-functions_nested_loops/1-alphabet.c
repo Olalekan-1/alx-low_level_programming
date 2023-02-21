5bcec1d6c1d53fbdd2da48a09645b1afd9b0dd4b
Ahmed Olalekan <olalekanahmed6@gmail.com>
2023-02-21 16:39:48 +0100

    task 2

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,1 +1,14 @@
+#include "main.h"
 
+/**
+* print_alphabet - Print the alphabet in lower case
+* Return: void
+*/
+
+void print_alphabet(void)
+{	char c;
+	for (c = 'a'; c <= 'z' c++)
+	{	_putchar(c);
+	}
+	_putchar(10);
+}
