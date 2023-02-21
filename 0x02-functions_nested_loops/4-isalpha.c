b30f15d43186c777878507bfee7912c00cd9f5a8
Ahmed Olalekan <olalekanahmed6@gmail.com>
2023-02-21 19:10:17 +0100

    Task 4

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -8,10 +8,5 @@
 int _isalpha(int c)
 {
 
-if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
-{	return (0);
-}
-else
-{	return (1);
-}
+	return (((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z')));
 }
