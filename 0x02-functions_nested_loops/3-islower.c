6c16e5db977619adffd953bb6ce7cf740f9b7d0a
Ahmed Olalekan <olalekanahmed6@gmail.com>
2023-02-21 18:23:33 +0100

    Task 3

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+* _islower - To check if it is a lower character
+* @c: character to be checked
+* Return: return 1 if c is lower character, return 0 if otherwise
+*/
+
+int _islower(int c)
+{
+	if (c >= 'a' && c <= 'z')
+	{
+	return (1);
+	}
+	else
+	return (0);
+
+}
