55c10e8df9372fa9dabda9408544d8b83eeb11c5
Ahmed Olalekan <olalekanahmed6@gmail.com>
2023-02-23 10:42:33 +0100

    Task 1

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,14 +1,15 @@
 #include "main.h"
 
 /**
-_isupper - To check if c is an upper case
-@c: value to be checked
-Return: 1 if c is upper else return 0
+* _isupper - To check if c is an upper case
+* @c: value to be checked
+* Return: 1 if c is upper else return 0
 */
 
 int _isupper(int c)
-{	if (c >= 65 && c <= 90)
+{
+	if (c >= 65 && c <= 90)
 	{	return (1);
 	}
-	return (0);	
+	return (0);
 }
