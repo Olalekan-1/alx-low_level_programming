594134d74e28f349265ff8fe7498d5a0f05aabd1
Ahmed Olalekan <olalekanahmed6@gmail.com>
2023-02-23 18:32:20 +0100

    Task 8

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+* print_square - To prints square using #
+* @size: The size of the squre
+* Return: The squre
+*/
+
+void print_square(int size)
+{	int i, j;
+	if (size == 0)
+	{	_putchar(10);
+	}
+	else if (size == 2 || size == 10)
+	{
+	for (i = 0; i < size; i++)
+	{
+	for (j = 0; j < size; j++)
+	{	_putchar(35);
+	}
+	_putchar(10);
+	}
+	}
+}
