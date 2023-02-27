0be1b639ca16d41a030a6f743085a6ecc19ce305
Ahmed Olalekan <olalekanahmed6@gmail.com>
2023-02-27 16:03:11 +0100

    Task 1

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+* swap_int - To swap the value of two integer
+* @a: the first int to be swap
+* @b: the second int to be swap
+* Return: The vlues swapped
+*/
+
+void swap_int(int *a, int *b)
+{	*a = 42;
+	*b = 98;
+}
