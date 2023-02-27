4978b5984d8b27a039835e951e47aba00b48dc0b
Ahmed Olalekan <olalekanahmed6@gmail.com>
2023-02-27 18:44:05 +0100

    Task 2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+* _strlen - to check the length of a string
+* @s: the variable to check from
+* Return: the length of the string.
+*/
+
+int _strlen(char *s)
+{	int i = 0;
+	while (*s != '\0')
+	{	i++;
+	s++;
+	}
+	return (i);
+}
