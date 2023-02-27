bc9a96609a11c1a175ba97ba911b96a67d022228
Ahmed Olalekan <olalekanahmed6@gmail.com>
2023-02-27 22:50:11 +0100

    Task 5

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -10,14 +10,13 @@ void rev_string(char *s)
 {	int l, i;
 	l = 0;
 	i = 0;
-	char j;
 
 	while (s[i++])
 	l++;
 
 	for (i	= l - 1; i >= l / 2; i--)
 	{
-	j = s[i];
+	char j = s[i];
 	s[i] = s[l - i - 1];
 	s[l - i - 1] = j;
 	}
