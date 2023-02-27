093c1d912384d920ed6251afe82c4313b5c64eb2
Ahmed Olalekan <olalekanahmed6@gmail.com>
2023-02-27 22:41:45 +0100

    Task 5

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+* rev_string - To print string in reverse
+* @s: The variable to print from
+* Return: print string in reverse
+*/
+
+void rev_string(char *s)
+{	int l, i;
+	l = 0;
+	i = 0;
+	char j;
+
+	while (s[i++])
+	l++;
+
+	for (i	= l - 1; i >= l / 2; i--)
+	{
+	j = s[i];
+	s[i] = s[l - i - 1];
+	s[l - i - 1] = j;
+	}
+
+}
