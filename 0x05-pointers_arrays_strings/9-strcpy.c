7167d56f17a2b739703f4e5e7af73c75cd104c2e
Ahmed Olalekan <olalekanahmed6@gmail.com>
2023-02-28 05:30:47 +0100

    Task 9

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+* _strcpy - To coppy a string from source to a destn.
+* @src: The source return value;
+* @dest: The destination return value.
+* Return: 0 always sucess
+*/
+
+char *_strcpy(char *dest, char *src)
+{	int i;
+	for (i = 0; src[i] != '\0'; i++)
+	{	dest[i] = src[i];
+	}
+	dest[i++] = '\0';
+	return (dest);
+}
