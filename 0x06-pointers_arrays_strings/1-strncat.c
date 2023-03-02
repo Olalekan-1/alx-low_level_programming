5ce01e591e58355f27d4ab4dc2baf1f0a89f5fa4
Ahmed Olalekan <olalekanahmed6@gmail.com>
2023-03-02 07:29:12 +0100

    Task 1

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+* _strncat - To concatenate strings
+* @dest: The destination to append to
+* @src: The scr to be append
+* @n: the byte(s) to append
+* Return: the appended destination with n byte(s)
+*/
+
+char *_strncat(char *dest, char *src, int n)
+{	int i = 0;
+	int j = 0;
+
+	while (dest[i] != '\0')
+	{	i++;
+	}
+	while (src[j] != '\0' && j < n)
+	{	dest[i] = src[j];
+	i++;
+	j++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
