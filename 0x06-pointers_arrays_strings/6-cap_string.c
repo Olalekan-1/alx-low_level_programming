d6207faf37c6a1c0d778c0c4a219e0bb312a6a3e
Ahmed Olalekan <olalekanahmed6@gmail.com>
2023-03-02 23:03:35 +0100

    Task 6

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -8,8 +8,11 @@
 
 char *cap_string(char *a)
 {	int i = 0;
-	while (a[i++])
-	{	while (a[i++] >= 'a' && a[i++] <= 'z')
+
+	while (a[i])
+
+	{
+	while (a[i++] >= 'a' && a[i++] <= 'z')
 	i++;
 	if (a[i - 1] == ' ' ||
 	a[i - 1] == '\t' ||
