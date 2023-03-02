7a447bcfd52264c995b03f443f81b6d3c8f15b5f
Ahmed Olalekan <olalekanahmed6@gmail.com>
2023-03-02 10:34:03 +0100

    Task 2

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -10,10 +10,13 @@
 
 char *_strncpy(char *dest, char *src, int n)
 {	int i = 0;
-	while (src[i] != '\0' && i < n)
-	{	dest[i] = src[i];
-	i++;
-	}
+	int l = 0;
+
+	while (src[i++])
+	l++;
+	for (i = 0; src[i] && i < n; i++)
+	dest[i] = src[i];
+	for (i = l; i < n; i++)
 	dest[i] = '\0';
 	return (dest);
 }
