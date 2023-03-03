ecbee80794674ccfa58e24037d14dedadcff4d7b
Ahmed Olalekan <olalekanahmed6@gmail.com>
2023-03-03 10:27:37 +0100

    Task 7

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+* leet - To convert some stings to a leetspeak
+* @str: The string to print from
+* Return: The encode string
+*/
+
+char *leet(char *str)
+{	int i, j;
+	char s1[] = "AaEeOeTtLl";
+	char s2[] = "4433007711";
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+	for (j = 0; j < 10; j++)
+	{
+	if (str[i] == s1[j])
+	{
+	str[i] = s2[j];
+	}
+	}
+	}
+	return (str);
+}
