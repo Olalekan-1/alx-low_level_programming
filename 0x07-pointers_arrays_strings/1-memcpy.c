2a8d88f1d1fca69670bf06ae1e6d5e9db4456467
Ahmed Olalekan <olalekanahmed6@gmail.com>
2023-03-06 14:43:40 +0100

    Task 1

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -1,1 +1,18 @@
 #include "main.h"
+
+/**
+* _memcpy - To copy a memoery area
+* @dest: The destination to copy to
+* @src: The source to copy from
+* @n: The n bytes to copy
+* Return: to the destination the n bytes cpied from the src.
+*/
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	for (int i = 0; i < n; i++)
+	{
+	dest[i] = src[i];
+	}
+	return (dest);
+}	
