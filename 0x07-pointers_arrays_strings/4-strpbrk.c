8c8fae37d52fe46b8555e4d295f679101b24bad5
Ahmed Olalekan <olalekanahmed6@gmail.com>
2023-03-06 18:17:49 +0100

    Task 4

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+* _strpbrk - To search and return a character from string
+* @s: pointer to the string
+* @accept: The string to search
+* Return: the character if found else NULL
+*/
+
+char *_strpbrk(char *s, char *accept)
+{
+	while (*s != '\0')
+	{
+	while (*accept != '\0')
+	{
+	accept++;
+	if (*s == *accept)
+	{
+	return ((char *)s);
+	}
+	s++;
+	}
+	}
+	return (((void *)0));
+}
+
+
