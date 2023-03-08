d7b448d888c03df2b90d1def65235ff34a2a666c
Ahmed Olalekan <olalekanahmed6@gmail.com>
2023-03-08 17:32:10 +0100

    Task 4

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+* _pow_recursion - print the result of x**y
+* @x: the base
+* @y: the power
+* Return: x**y
+*/
+
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+	{
+	return (-1);
+	}
+	if (y == 0)
+	{
+	return (1);
+	}
+	if (y == 1)
+	{
+	return (x);
+	}
+	return (x * _pow_recursion(x, y - 1));
+}
