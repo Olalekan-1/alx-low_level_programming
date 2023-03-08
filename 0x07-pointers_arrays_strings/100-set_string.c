96ff3f49bd912e14a17af7aea3207f38cedc7cde
Ahmed Olalekan <olalekanahmed6@gmail.com>
2023-03-08 05:20:09 +0100

    task 8

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+* set_string - To set the value of pointer to another pointer
+* @s: source pointer
+* @to: destination pointer
+* Return: the value of the pointer
+*/
+
+
+
+
+void set_string(char **s, char *to)
+{	*s = to;
+}
