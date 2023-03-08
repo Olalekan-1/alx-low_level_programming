0bf7a3e1cd3bfcb2c2ac84c39591e8b5d5f0d7c2
Ahmed Olalekan <olalekanahmed6@gmail.com>
2023-03-08 19:10:29 +0100

    task 5

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+int _sqrt(int n, int i);
+
+/**
+* _sqrt_recursion - to print the sqrt of a numnber
+* @n: the number to find its sqrt
+* Return: the sqrt
+*/
+
+int _sqrt_recursion(int n)
+{	(_sqrt(n, 1));
+}
+
+/**
+* _sqrt - To calculate the sqrt of a number
+* @n: The number to print it sqrt
+* @i: use to find the sqrt
+* Return: Tggg
+*/
+
+int _sqrt(int n, int i)
+{	int j = i * i;
+	if (j > n)
+	{
+	return (-1);
+	}
+	if (j == n)
+	{
+	return (i);
+	}
+	else
+	return (_sqrt(n, i + 1));
+}
