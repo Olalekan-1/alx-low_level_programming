a0ea104121afd499c32fda8bc4d6ba8eccd977d3
Ahmed Olalekan <olalekanahmed6@gmail.com>
2023-03-16 09:07:04 +0100

    Task 0

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+* malloc_checked - To allocate and check the memory allocation
+* @b: size of memor to be allocated
+* Return: pointer to the memory allocated else exit value
+*/
+
+void *malloc_checked(unsigned int b)
+{	int *ptr;
+
+	ptr = malloc(sizeof(int) * b);
+	if (ptr == ((void *)0))
+	{
+		exit(98);
+	}
+	return (ptr);
+}
---
+++ 0x0C-more_malloc_free/README.md
@@ -0,0 +1,1 @@
+ This is more on how on how to use the malloc() and the free() in C
