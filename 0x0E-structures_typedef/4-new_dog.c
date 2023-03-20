ee6dba43a7cf153b5f725aa205c3a46153a4b86b
Ahmed Olalekan <olalekanahmed6@gmail.com>
2023-03-20 22:27:04 +0100

    Task 3

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -13,7 +13,10 @@ dog_t *new_dog(char *name, float age, char *owner)
 {	dog_t *newdog;
 	newdog = malloc(sizeof(dog_t));
 	if (newdog == ((void *)0))
-	return (((void *)0));
+	{
+		free(newdog);
+		return (((void *)0));
+	}
 	newdog->name = name;
 	newdog->age = age;
 	newdog->owner = owner;
