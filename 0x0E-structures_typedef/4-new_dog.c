cd376bcb5501175d9d693bb99f60f2cdf6fc2e9e
Ahmed Olalekan <olalekanahmed6@gmail.com>
2023-03-20 22:02:01 +0100

    task 4

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include "dog.h"
+
+/**
+* new_dog - dog data
+* @name: name of the dog
+* @age: age of the dog
+* @owner: name of the owner
+* Return: pointer to the newdog
+*/
+
+dog_t *new_dog(char *name, float age, char *owner)
+{	dog_t *newdog;
+	newdog = malloc(sizeof(dog_t));
+	if (newdog == ((void *)0))
+	return (((void *)0));
+	newdog->name = name;
+	newdog->age = age;
+	newdog->owner = owner;
+	return (newdog);
+}
