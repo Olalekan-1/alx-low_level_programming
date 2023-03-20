e601dfcc7f833827627dcef5e57b3f6bce659967
Ahmed Olalekan <olalekanahmed6@gmail.com>
2023-03-20 12:29:49 +0100

    task 2

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+* main - To print the file name
+* @void: no parameter
+* Return: 0 always 0
+*/
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
