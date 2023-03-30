3beb61110c510f8a0b24baa52ac2855e86b3bd6e
Ahmed Olalekan <olalekanahmed6@gmail.com>
2023-03-30 14:05:05 +0100

    task 5

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,13 @@
+#include "lists.h"
+
+/**
+* print - function that print word
+* Return: The word
+* @void: ...
+*/
+
+void print(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
