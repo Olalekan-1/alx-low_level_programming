e6f0d2d5e37f124ad2e7228a8a01019659809527
Ahmed Olalekan <olalekanahmed6@gmail.com>
2023-04-04 09:45:30 +0100

    Task 11

--- 0x13-more_singly_linked_lists/100-reverse_listint.c
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -6,4 +6,21 @@
  * Return: reversed node;
  */
 
+listint_t *reverse_listint(listint_t **head)
+{	listint_t *nextnode = NULL;
+	listint_t *previousnode = NULL;
 
+	if (*head == NULL || (*head)->next == NULL)
+	{
+		return (NULL);
+	}
+	while (*head != NULL)
+	{
+		nextnode = (*head)->next;
+		(*head)->next = previousnode;
+		previousnode = *head;
+		*head = nextnode;
+	}
+	*head = previousnode;
+	return (*head);
+}
