25ef6e7ae21afd3f0743165fd692d54274f5c1a3
Ahmed Olalekan <olalekanahmed6@gmail.com>
2023-04-08 12:42:26 +0100

    Task 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -30,11 +30,6 @@ int create_file(const char *filename, char *text_content)
 
 	length = _strlen(text_content);
 
-	if (filename == NULL)
-	{
-		return (-1);
-	}
-
 	file_des = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
 	if (file_des == -1)
 	{
