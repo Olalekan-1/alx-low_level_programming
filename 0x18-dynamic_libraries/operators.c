0aa0290b8a0a455a91c50c3bbd90b95462008619
Ahmed Olalekan <olalekanahmed6@gmail.com>
2023-05-22 15:10:00 +0100

    i miss python instead not C :(

---
+++ 0x18-dynamic_libraries/operators.c
@@ -0,0 +1,75 @@
+#include "main.h"
+
+/**
+ * add - function to add two numbers
+ * @a: first operand
+ * @b: second operand
+ * Return: Addtion of two number
+ */
+
+int add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * sub - function to sub a num from a num
+ * @a: The number to sub from
+ * @b: The number to be sub
+ * Return: the subtracted number
+ */
+
+int sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * mul - The multiplication of two integers
+ * @a: First int
+ * @b: Second int
+ * Return: The result of multiplication
+ */
+
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
+
+
+
+/**
+ * div - Division of two number
+ * @a: dividend
+ * @b: divisor
+ * Return: the division of dividend and divisor
+ */
+
+int div(int a, int b)
+{
+	return (a / b);
+}
+
+
+/**
+ * mod - function to get the remainder of a division
+ * @a: dividend
+ * @b: divisor:
+ * Return: the remainder
+ */
+
+int mod(int a, int b)
+{
+	return (a % b);
+}
+
+
+
+
+
+
+
+
+
+
