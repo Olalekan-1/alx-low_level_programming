11ee6e8bc019ad1fb9e4466f249dcf46fd0e2824
Ahmed Olalekan <olalekanahmed6@gmail.com>
2023-05-22 17:09:47 +0100

    Task 3

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,1 +1,2 @@
-
+#!/bin/bash
+LD_PRELOAD=/$PWD/gm.so ./gm 9 8 10 24 75 9
--- 0x18-dynamic_libraries/gm.c
+++ 0x18-dynamic_libraries/gm.c
@@ -3,7 +3,8 @@
 #include <stdlib.h>
 int printf(const char *format, ...)
 {
-	write(1, " 9 8 10 24 75 - 9\n", 17);
-	write(1, "conngratulations, you win the jackpot!\n", 38);
+	write(1, "9 8 10 24 75 -9\n", 17);
+	write(1, "Conngratulations, you win the jackpot!\n", 38);
+	putchar(10);
 	exit(0);
 }
