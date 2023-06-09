b88173c583551653ea2f492ef6e95ec2120065ea
Ahmed Olalekan <olalekanahmed6@gmail.com>
2023-06-09 09:14:18 +0100

    Task 0 update

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -13,11 +13,19 @@ hash_table_t *hash_table_create(unsigned long int size)
 	unsigned long int i = 0;
 
 	table = malloc(sizeof(hash_table_t));
+	if (!table)
+	{
+		free(table);
+		return (NULL);
+	}
 	table->size = size;
 	table->array = malloc(sizeof(hash_node_t *) * size);
 
 	if (table->array == NULL)
+	{
+		free(table);
 		return (NULL);
+	}
 	for (i = 0; i < size; i++)
 	{
 		table->array[i] = NULL;
