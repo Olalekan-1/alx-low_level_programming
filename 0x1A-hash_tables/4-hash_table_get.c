e8201859643a0020efee43e5df3479ca065d151d
Ahmed Olalekan <olalekanahmed6@gmail.com>
2023-06-09 06:58:10 +0100

    retrieve element

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -16,7 +16,7 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 		return (NULL);
 
 	index = key_index((const unsigned char *)key, ht->size);
-	
+
 	while (ht->array[index])
 	{
 	if (ht->array[index] != NULL && strcmp(ht->array[index]->key, key) == 0)
