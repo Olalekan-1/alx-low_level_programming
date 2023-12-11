68b77b9c7acd30001f288d41d5ad314bb90b0843
Ahmed Olalekan <olalekanahmed6@gmail.com>
2023-12-11 17:01:43 +0100

    add solution

---
+++ 0x1E-search_algorithms/0-linear.c
@@ -0,0 +1,36 @@
+#include "search_algos.h"
+
+
+/**
+ * linear_search - implementation of linear search
+ * @array: pointer to the array to search for element
+ * @size: size of the array tobe search
+ * @value: The element to search in the array
+ * Return: The index of the serched elememt if found
+ * else -1
+ */
+
+int linear_search(int *array, size_t size, int value)
+{
+	size_t i;
+	int found = 0;
+	size_t index;
+
+	for (i = 0; i < size; i++)
+	{
+		if (array[i] == value)
+		{
+			found = 1;
+			printf("%i\n", array[i]);
+			/* printf("%li\n", i);*/
+			index = i;
+			break;
+		}
+		else
+			printf("%i\n", array[i]);
+	}
+
+	if (found == 0)
+		return (-1);
+	return (index);
+}
---
+++ 0x1E-search_algorithms/README.md
@@ -0,0 +1,1 @@
+# Solution to Seraching algorithms tasks
